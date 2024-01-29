d65c39d466141ef2cec9fa2cd23470d3ebe4dc62
Flexify <support@flexify.io>
2024-01-29 10:57:21 +0000

    v2.15.0-SNAPSHOT build 1438

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "flexify-api"
-VERSION = "2.15.0.dev1435"
+VERSION = "2.15.0.dev1438"
 # To install the library, run the following
 #
 # python setup.py install
